dc1e26643ce49421d4bd3b0aec167f4fa5e0df7d
Ahmed Ayman <ahmedayman9113@gmail.com>
2025-04-10 19:37:23 +0200

    tryiing

--- server/api/index.js
+++ server/api/index.js
@@ -1,2 +1,12 @@
 import app from "../index.js";
+import express from 'express';
+import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
+
+const app = express();
+
+app.get('/private', ClerkExpressRequireAuth(), (req, res) => {
+  res.json({ message: `Hello, ${req.auth.userId}!` });
+});
+
+
 export default app
